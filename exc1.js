ba7ac4426adfa11cd7c3303f5fe4ce095d296571
abhisheksharm-3 <95611197+abhisheksharm-3@users.noreply.github.com>
2023-03-19 15:59:12 +0530

    Delete exc1.js

--- exc1.js
+++
@@ -1,7 +0,0 @@
-var mydiv = document.getElementById("mydiv");
-mydiv.addEventListener("mouseover", function () {
-    mydiv.innerText="Can I help you?";
-});
-mydiv.addEventListener("mouseout", function () {
-    mydiv.innerText="Hello World!";
-});
